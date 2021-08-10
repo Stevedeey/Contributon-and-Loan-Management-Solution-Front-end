1b4b7f23a151888d6e84d6d0c47f75557683f8f7
stevedeey <stvolutosin69@gmail.com>
2021-08-10 18:22:40 +0100

    freaky 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Freaky</h1>
+        <h2>Just trying to test stuff</h2>
         <p>
 
           Edit <code>src/App.js</code> and save to reload.
